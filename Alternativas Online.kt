c897546f18d7979f2d0d81ee1cc963418b7a9121
KarenAideeMarin <146773995+KarenAideeMarin@users.noreply.github.com>
2023-10-03 21:39:13 -0600

    Create Alternativas Online.kt

---
+++ Alternativas Online.kt
@@ -0,0 +1,9 @@
+/**
+* You can edit, run, and share this code.
+* play.kotlinlang.org
+*/
+fun main() {
+    print("¡Hola UPIICSA!")
+    print("Karen Aidee Marin")
+    println("Imprimiendo en println")
+    println("Otro println")
